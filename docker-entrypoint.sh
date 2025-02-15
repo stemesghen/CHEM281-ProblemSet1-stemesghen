cf923d032a534e7a81f829df6d212ca8bbb9dd8f
Sabrina Temesghen <stemesghen@berkeley.edu>
2025-02-14 22:01:58 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,11 +10,10 @@ elements="$3"
 # Construct the BSE API URL
 download_url="${BASE_URL}/api/basis/${basis}/format/${format}/?elements=${elements}"
 
-# Create output directory
-mkdir -p /output
+#download into readme
+curl -s "$download_url" > "/README.md"
 
-# Download the file directly without error checking
-curl -s "$download_url" -o "/output/${basis}.${format}"
+echo "Downloaded ${basis}.${format} and saved to README.md"
 
 # Print a confirmation message
 echo "Downloaded ${basis}.${format} to /output/"
