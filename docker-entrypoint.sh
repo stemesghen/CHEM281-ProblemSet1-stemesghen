3ae3863497689eaa1e0af5bbcbee2abbf6413e0b
Sabrina Temesghen <stemesghen@berkeley.edu>
2025-02-14 20:36:39 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,21 @@
 BASE_URL="https://www.basissetexchange.org"
 
 #input arguments
-basis="$1" 
-format="$2" 
-elements="$3" 
+basis="$1"
+format="$2"
+elements="$3"
 
 #url Bse api
 
 download_url="${BASE_URL}/api/basis/${basis}/format/${format}/?elements=${elements}"
 
-#execute 
-exec curl -s "$download_url"
+# output directory in container
+mkdir -p /output
+
+# Download and save the file
+curl -s "$download_url" -o "/output/${basis}.${format}"
+
+# Print output completed
+echo "Downloaded ${basis}.${format} to /output/"
+
 
